e7c2fae8fd06e361a788fffcd845abb3253d081e
Joe Rivera <dvs_joe@hotmail.com>
2023-06-14 18:47:28 -0400

    added new code to fix error on server

--- Develop/server.js
+++ Develop/server.js
@@ -1,92 +1,42 @@
 const express = require('express');
 const path = require('path');
-let notesDb=require(`./db/db.json`);
-const fs=require(`fs`);
-const PORT = 3001;
+let notesDb = require(`./db/db.json`);
+const fs = require(`fs`);
 const { v4: uuidv4 } = require('uuid');
-const serveStatic = require('serve-static');
-
-
+const { stringify } = require('querystring');
+const PORT = 3001;
 const app = express();
-
-//middleaware
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(serveStatic('public'));
-
-
-// default "home page"
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
-// notes html
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
-// GET request for notes
 app.get('/api/notes', (req, res) => {
-  // Inform the client
-  res.status(200).json(notesDb);
-  // Log our request to the terminal
+  const data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
+  res.status(200).json(data);
   console.info(`${req.method} request received to get notes`);
 });
-
-// GET a single note
-// app.get('/api//:noteId', (req, res) => {
-//   if (req.params.noteId) {
-//     console.info(`${req.method} request received to get a single a review`);
-//     const noteId = req.params.noteId;
-//     for (let i = 0; i < notes.length; i++) {
-//       const currentNote = notes[i];
-//       if (currentNote.noteId === noteId) {
-//         res.json(currentNote);
-//         return;
-//       }
-//     }
-//     res.status(404).send('Note not found');
-//   } else {
-//     res.status(400).send('Note ID not provided');
-//   }
-// });
-
-
-// POST request for notes
 app.post('/api/notes', (req, res) => {
-  // Inform the client that their POST request was received
   res.json(`${req.method} request received to add a note`);
-
-  // Log our request to the terminal
   console.info(`${req.method} request received to add a note`);
-  
-  // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
-  // If all the required properties are present
-    if (title && text) {
-  // Variable for the object we will save
-  const newNote = {
-    id: uuidv4(), // Make sure to install and import the 'uuid' module
-    title: 'Sample Note',
-    text: 'This is a sample note',
-  };
-    // Obtain existing notes
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
       if (err) {
         console.error(err);
       } else {
-        // Convert string into JSON object
         const parsedNotes = JSON.parse(data);
-
-        // Add a new note
         parsedNotes.push(newNote);
-
-        // Write updated note back to the file
-        fs.writeFile(
-          './db/db.json',
-          JSON.stringify(parsedNotes, null, 4),
+        fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4),
           (writeErr) =>
             writeErr
               ? console.error(writeErr)
@@ -94,37 +44,23 @@ app.post('/api/notes', (req, res) => {
         );
       }
     });
-
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-    
-    console.log(response);
-    res.status(201).json(response);
-    
-
-    console.log(response);
-    res.status(201).json(response);
+    ;
+    res.status(200)
   } else {
     res.status(500).json('Error in posting note');
   }
-
-
-
 });
-
-// DELETE request for upvotes
 app.delete('/api/notes/:id', (req, res) => {
-  // Inform the client
-  res.json(`${req.method} request received to delete note`);
-
-  // Log our request to the terminal
-  console.info(`${req.method} request received to delete note`);
+  const data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
+  const filterData = data.filter(note => note.id !== req.params.id)
+  fs.writeFile('./db/db.json', JSON.stringify(filterData, null, 4),
+    (writeErr) =>
+      writeErr
+        ? console.error(writeErr)
+        : console.info('Successfully deleted note!')
+  )
+    res.status(200).json({ok: true})
 });
-
-
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
 );
-
